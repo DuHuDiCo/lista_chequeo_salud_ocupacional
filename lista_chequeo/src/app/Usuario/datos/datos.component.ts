dbcf6f2d017f3f3793ad62fdea32183265f9d9af
Duvan Diaz <dudicodiaz@gmail.com>
2022-11-08 14:50:25 -0500

    comit 08/11/2022

--- lista_chequeo/src/app/Usuario/datos/datos.component.ts
+++ lista_chequeo/src/app/Usuario/datos/datos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-datos',
@@ -11,94 +12,183 @@ export class DatosComponent implements OnInit {
 
   formatoInspeccion = {
     codigo: "",
-    fecha: '',
+    fecha: '2021-03-01',
     realizdoBy: '',
     fecha_inspeccion: '',
     punto_venta: '',
     sustanciasQuimicas: {},
-    peligrosElectricos:{},
-    peligrosMecanicos:{},
-    peligrosLocativos:{},
-    emergencias:{},
-    ordenAseo:{},
-    saneamientoBasico:{}
+    peligrosElectricos: {},
+    peligrosMecanicos: {},
+    peligrosLocativos: {},
+    emergencias: {},
+    ordenAseo: {},
+    saneamientoBasico: {}
   }
 
   sustanciasQuimicas = {
-    hojas_serguridad_sustancias_quimicas: false,
-    sustancias_quimacas_etiquetadas: false,
-    gafas_seguridad_guantes_nitrilo: false,
-    sustancias_quimicas_almacenadas: false,
+    hojas_seguridad_sustancias_quimicas: '',
+    sustancias_quimicas_etiquetadas: '',
+    gafas_seguridad_guantes_nitrilo: '',
+    sustancias_quimicas_almacenadas: '',
     observaciones: '',
     evidencias: []
   }
 
   peligrosElectricos = {
-    cables_buen_estado_entubados: false,
-    empalmes_conexiones_buen_estado: false,
-    tomas_buen_estado_tapa_proteccion: false,
-    cajas_breakers_sin_sobrecarga_señaladas: false,
-    tableros_caja_breakers_sin_material_combustible: false,
+    cables_buen_estado_entubados: '',
+    empalmes_conexiones_buen_estado: '',
+    tomas_buen_estado_tapa_proteccion: '',
+    cajas_breakers_sin_sobrecarga_senaladas: '',
+    tableros_caja_breakers_sin_material_combustible: '',
     observaciones: '',
     evidencias: []
   }
 
   peligrosMecanicos = {
-    equipos_herramientas_buen_estado: false,
+    equipos_herramientas_buen_estado: '',
     observaciones: '',
     evidencias: []
   }
 
   peligrosLocativos = {
-    luminarias_buen_estado: false,
-    muros_buen_estado: false,
-    escaleras_buen_estado: false,
-    pisos_buen_estado: false,
-    ventanas_puertas_buen_estado: false,
-    techos_buen_estado: false,
-    areas_circulacion_despejadas: false,
-    silla_escritorios_buen_estado: false,
-    divisiones_modulares_escritorio_cajones_buenas_condiciones: false,
+    luminarias_buen_estado: '',
+    muros_buen_estado: '',
+    escaleras_buen_estado: '',
+    pisos_buen_estado: '',
+    ventanas_puertas_buen_estado: '',
+    techos_buen_estado: '',
+    areas_circulacion_despejadas: '',
+    silla_escritorios_buen_estado: '',
+    divisiones_modulares_escritorio_cajones_buenas_condiciones: '',
     observaciones: '',
     evidencias: []
   }
 
   emergencias = {
-    ruta_evacuacion_señalizada_libre_obstaculos: false,
-    salida_emergencia_punto_encuentro_señalizadas_despejadas: false,
-    lista_telefonos_emergencia_publicada_socializada: false,
-    extintores_buen_estado_recargados_señalizados_libre_obstaculos: false,
-    camilla_emergencia_buen_estado_cuello_inmovilizador: false,
-    botiquin_buen_estado_elementos_necesarios: false,
+    ruta_evacuacion_senalizada_libre_obstaculos: '',
+    salida_emergencia_punto_encuentro_senalizadas_despejadas: '',
+    lista_telefonos_emergencia_publicada_socializada: '',
+    extintores_buen_estado_recargados_senalizados_libre_obstaculos: '',
+    camilla_emergencia_buen_estado_cuello_inmovilizador: '',
+    botiquin_buen_estado_elementos_necesarios: '',
     observaciones: '',
     evidencias: []
   }
 
   ordenAseo = {
-    punto_venta_buen_estado_aseo_mantenimiento: false,
-    archivadores_organizados_documento_libros_almacenados: false,
+    punto_venta_buen_estado_aseo_mantenimiento: '',
+    archivadores_organizados_documento_libros_almacenados: '',
     observaciones: '',
     evidencias: []
   }
 
   saneamientoBasico = {
-    puntos_ecologicos_residuos: false,
-    guantes_nitrilo_caucho: false,
-    cuarto_residuos_limpio: false,
-    servicio_sanitario_optimas_condiciones: false,
-    baños_papel_higienico_jabon_toallas_papeleras: false,
-    insectos_roedores_fumigado_areas: false,
-    observaciones: false,
+    puntos_ecologicos_residuos: '',
+    guantes_nitrilo_caucho: '',
+    cuarto_residuos_limpio: '',
+    servicio_sanitario_optimas_condiciones: '',
+    banos_papel_higienico_jabon_toallas_papeleras: '',
+    insectos_roedores_fumigado_areas: '',
+    observaciones: '',
     evidencias: []
   }
 
-  constructor() { }
+  archivo = {
+    "archivo": '',
+    "ruta": ''
+  }
+
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
 
+  public file(event: any, name: any, cate: any) {
+    var img = event.target.files[0];
+    this.extraerBase64(img).then((imagen: any) => {
+      this.archivo.ruta = imagen.base;
+      this.archivo.archivo = name + ".jpg";
+      switch (cate) {
+        case "sustanciasQuimicas":
+          let file = this.archivo;
+          (this.sustanciasQuimicas.evidencias as any[]).push(file);
 
-  print(){
+
+          break;
+        case "peligrosElectricos":
+          (this.peligrosElectricos.evidencias as any[]).push(this.archivo);
+
+          break;
+        case "peligrosMecanicos":
+          (this.peligrosMecanicos.evidencias as any[]).push(this.archivo);
+          break;
+        case "peligrosLocativos":
+          (this.peligrosLocativos.evidencias as any[]).push(this.archivo);
+          break;
+        case "emergencias":
+          (this.emergencias.evidencias as any[]).push(this.archivo);
+          break;
+        case "ordenAseo":
+          (this.ordenAseo.evidencias as any[]).push(this.archivo);
+          break;
+        case "saneamientoBasico":
+          (this.saneamientoBasico.evidencias as any[]).push(this.archivo);
+          break;
+
+      }
+    })
+
+    this.archivo = {
+      "archivo": '',
+      "ruta": ''
+    }
+
+
+
+  }
+  print() {
     console.log(this.sustanciasQuimicas)
   }
+
+
+  public extraerBase64 = async ($event: any) => new Promise((resolve, reject): any => {
+    try {
+      const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          base: null
+        });
+      };
+
+    } catch (e) {
+      return null;
+    }
+  })
+
+
+  public guardarFormato() {
+    this.formatoInspeccion.sustanciasQuimicas = this.sustanciasQuimicas;
+    this.formatoInspeccion.peligrosElectricos = this.peligrosElectricos;
+    this.formatoInspeccion.peligrosMecanicos = this.peligrosMecanicos;
+    this.formatoInspeccion.peligrosLocativos = this.peligrosLocativos;
+    this.formatoInspeccion.emergencias = this.emergencias;
+    this.formatoInspeccion.ordenAseo = this.ordenAseo;
+    this.formatoInspeccion.saneamientoBasico = this.saneamientoBasico;
+
+    console.log(this.formatoInspeccion)
+
+  }
+
+
+
+
 }
