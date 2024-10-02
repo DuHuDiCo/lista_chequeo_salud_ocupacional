8386609a240fee794e355a13ddf37aabc29f8533
Duvan Diaz <dudicodiaz@gmail.com>
2024-10-02 16:15:23 -0500

    commit

--- lista_chequeo/src/app/Servicio/url.ts
+++ lista_chequeo/src/app/Servicio/url.ts
@@ -1,4 +1,4 @@
 //  let base= "http://201.184.154.82/checklist/api/v1/formato"
-let base = "http://192.168.1.171:8000/checklist/api/v1/formato"
+let base = "https://electrohogarbackendlocal.duckdns.org/checklist/api/v1/formato"
 // let base= "http://localhost:8080/api/v1/formato"
 export default base;
