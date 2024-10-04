4d3c95646e156aecb080e2d4a4057d156779eba2
Duvan Diaz <dudicodiaz@gmail.com>
2024-10-04 17:45:56 -0500

    commit

--- lista_chequeo/src/app/Servicio/url.ts
+++ lista_chequeo/src/app/Servicio/url.ts
@@ -1,4 +1,4 @@
-//  let base= "http://201.184.154.82/checklist/api/v1/formato"
-let base = "https://electrohogarbackendlocal.duckdns.org/checklist/api/v1/formato"
+  let base= "https://electrohogarbackend.almacenelectrohogar.com/api/v1/formato"
+//let base = "https://electrohogarbackendlocal.duckdns.org/checklist/api/v1/formato"
 // let base= "http://localhost:8080/api/v1/formato"
 export default base;
